66b799c8699a509b7568466d94861c07d778d5a1
Thiago Souza <souzaalvesthiago99@gmail.com>
2023-06-28 14:38:44 -0300

    Configurando ambiente de trabalho

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Teste from './Teste'
+import EstiloGlobal from './styles'
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Teste />
+        <EstiloGlobal />
+      </>
+    </div>
+  )
+}
+
+export default App
