7c6e2082e9ac5a187eddc110d7284a142691991b
Thiago Souza <souzaalvesthiago99@gmail.com>
2023-07-10 11:21:59 -0300

    Criando e estilizando sessão projetos

---
+++ src/Components/Projeto/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Card = styled.div`
+  border: 1px solid #c1c1c1;
+  padding: 16px;
+`
+
+export const LinkBotao = styled.a`
+  color: #fff;
+  background-color: #4476bf;
+  padding: 8px;
+  font-size: 14px;
+  display: inline-block;
+  margin-top: 24px;
+  cursor: pointer;
+`
--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,8 +1,31 @@
 import Titulo from '../../Components/Titulo'
+import Projeto from '../../Components/Projeto'
+
+import Lista from './styles'
 
 const Projetos = () => (
   <section>
     <Titulo>Projetos</Titulo>
+    <Lista>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+      <li>
+        <Projeto />
+      </li>
+    </Lista>
   </section>
 )
 
